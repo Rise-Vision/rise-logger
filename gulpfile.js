7c7edf229669686dac06abf1e351b0fc8afcc033
Stuart Lees <stuartlees@gmail.com>
2016-12-22 15:47:49 -0500

    Implement use of ESLint and remove JSHint

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,57 @@
-(function (console) {
+/* global require */
+
+( function( console ) {
   "use strict";
 
-  var bower = require("gulp-bower");
-  var bump = require("gulp-bump");
-  var colors = require("colors");
-  var del = require("del");
-  var gulp = require("gulp");
-  var jshint = require("gulp-jshint");
-  var runSequence = require("run-sequence");
-  var uglify = require('gulp-uglify');
-  var wct = require("web-component-tester").gulp.init(gulp);
+  var bower = require( "gulp-bower" ),
+    bump = require( "gulp-bump" ),
+    del = require( "del" ),
+    eslint = require( "gulp-eslint" ),
+    gulp = require( "gulp" ),
+    runSequence = require( "run-sequence" ),
+    wct = require( "web-component-tester" ).gulp.init( gulp ); // eslint-disable-line
 
-  gulp.task("clean-bower", function(cb){
-    del(["./bower_components/**"], cb);
-  });
+  gulp.task( "clean-bower", function( cb ) {
+    del( [ "./bower_components/**" ], cb );
+  } );
 
-  gulp.task("lint", function() {
-    return gulp.src("./*.html")
-      .pipe(jshint.extract("always"))
-      .pipe(jshint())
-      .pipe(jshint.reporter("jshint-stylish"))
-      .pipe(jshint.reporter("fail"));
-  });
+  gulp.task( "lint", function() {
+    return gulp.src( [ "./*.js", "test/**/*.html" ] )
+      .pipe( eslint() )
+      .pipe( eslint.format() )
+      .pipe( eslint.failAfterError() );
+  } );
 
   // ***** Primary Tasks ***** //
-  gulp.task("bower-clean-install", ["clean-bower"], function(cb){
-    return bower().on("error", function(err) {
-      console.log(err);
+  gulp.task( "bower-clean-install", [ "clean-bower" ], function( cb ) {
+    return bower().on( "error", function( err ) {
+      console.log( err );
       cb();
-    });
-  });
+    } );
+  } );
 
-  gulp.task("bump", function(){
-    return gulp.src(["./package.json", "./bower.json"])
-      .pipe(bump({type:"patch"}))
-      .pipe(gulp.dest("./"));
-  });
+  gulp.task( "bump", function() {
+    return gulp.src( [ "./package.json", "./bower.json" ] )
+      .pipe( bump( { type: "patch" } ) )
+      .pipe( gulp.dest( "./" ) );
+  } );
 
-  gulp.task("test", function(cb) {
-    runSequence("test:local", cb);
-  });
+  gulp.task( "test", function( cb ) {
+    runSequence( "test:local", cb );
+  } );
 
-  gulp.task("build", function (cb) {
-    runSequence("lint", cb);
-  });
+  gulp.task( "build", function( cb ) {
+    runSequence( "lint", cb );
+  } );
 
-  gulp.task("default", [], function() {
-    console.log("********************************************************************".yellow);
-    console.log("  gulp bower-clean-install: delete and re-install bower components".yellow);
-    console.log("  gulp bump: increment the version".yellow);
-    console.log("  gulp test: run unit and integration tests".yellow);
-    console.log("  gulp build: build component".yellow);
-    console.log("********************************************************************".yellow);
+  gulp.task( "default", [], function() {
+    console.log( "********************************************************************".yellow );
+    console.log( "  gulp bower-clean-install: delete and re-install bower components".yellow );
+    console.log( "  gulp bump: increment the version".yellow );
+    console.log( "  gulp test: run unit and integration tests".yellow );
+    console.log( "  gulp build: build component".yellow );
+    console.log( "********************************************************************".yellow );
     return true;
-  });
+  } );
 
-})(console);
+} )( console );
