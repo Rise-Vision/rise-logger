3337e372971ff3d6881d24e3bf714b6e7d4a3006
Stuart Lees <stuartlees@gmail.com>
2017-02-01 11:08:42 -0500

    Inject version into component code

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@
     del = require( "del" ),
     eslint = require( "gulp-eslint" ),
     gulp = require( "gulp" ),
+    htmlreplace = require( "gulp-html-replace" ),
     runSequence = require( "run-sequence" ),
     wct = require( "web-component-tester" ).gulp.init( gulp ); // eslint-disable-line
 
@@ -20,6 +21,19 @@
       .pipe( eslint() )
       .pipe( eslint.format() )
       .pipe( eslint.failAfterError() );
+  } );
+
+  gulp.task( "version", function() {
+    var pkg = require( "./package.json" );
+
+    gulp.src( "./rise-logger.html" )
+      .pipe( htmlreplace( {
+        "version": {
+          src: pkg.version,
+          tpl: "<script>var loggerVersion = \"%s\";</script>"
+        }
+      }, { keepBlockTags: true } ) )
+      .pipe( gulp.dest( "./" ) );
   } );
 
   // ***** Primary Tasks ***** //
@@ -40,7 +54,7 @@
     runSequence( "test:local", cb );
   } );
 
-  gulp.task( "build", function( cb ) {
+  gulp.task( "build", [ "version" ], function( cb ) {
     runSequence( "lint", cb );
   } );
 
